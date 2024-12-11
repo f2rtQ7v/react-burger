47adbeae54829374b284b253a26e2ea43c85ca44
Victor <xD34F@yandex.ru>
2024-12-11 11:34:20 +0300

    Link component fixed

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,6 +1,7 @@
 import styles from './link.module.css';
 
-const Link = ({ children, className, ...props }) =>
-  <span className={`${styles.link} ${className || ''}`} {...props}>{children}</span>
-
-export default Link;
+export default ({ children, className, ...props }) => (
+  <span className={`${styles.link} ${className || ''}`} {...props}>
+    {children}
+  </span>
+);
