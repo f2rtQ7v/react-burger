4de4a4dda0122d1e416a440effa572ad5dcd8ff5
Victor <xD34F@yandex.ru>
2024-12-15 11:44:21 +0300

    добавлено неактивное состояние для ссылок в меню

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -3,18 +3,18 @@ import Link from '../link/link.tsx';
 import styles from './app-header.module.css';
 
 const links = [
-  { Icon:  BurgerIcon, type:   'primary', text:    'Конструктор', id: 'burgerConstructor' },
-  { Icon:    ListIcon, type: 'secondary', text:  'Лента заказов', id:            'orders' },
-  { Icon: ProfileIcon, type: 'secondary', text: 'Личный кабинет', id:           'account' },
+  { Icon:  BurgerIcon, text:    'Конструктор', id: 'burgerConstructor' },
+  { Icon:    ListIcon, text:  'Лента заказов', id:            'orders', disabled: true },
+  { Icon: ProfileIcon, text: 'Личный кабинет', id:           'account', disabled: true },
 ];
 
 export default function AppHeader() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        {links.map(({ Icon, type, text, id }) => (
-          <Link key={id} className={styles[id]}>
-            <Icon type={type} />
+        {links.map(({ Icon, text, id, disabled = false }) => (
+          <Link key={id} className={styles[id]} disabled={disabled}>
+            <Icon type={disabled ? 'secondary' : 'primary'} />
             <span>{text}</span>
           </Link>
         ))}
--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,8 +1,12 @@
 import styles from './link.module.css';
 
-export default function Link({ children, className, ...props }) {
+export default function Link({ children, className, disabled, ...props }) {
+  const classes = [ styles.link, disabled && styles.disabled, className ]
+    .filter(Boolean)
+    .join(' ');
+
   return (
-    <span className={`${styles.link} ${className || ''}`} {...props}>
+    <span className={classes} {...props}>
       {children}
     </span>
   );
