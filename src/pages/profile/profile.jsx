ffb46c5170b5ee56aeb4035f2e727cfbd43221f7
Victor <xD34F@yandex.ru>
2025-02-01 04:59:05 +0300

    profile links fixed

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -7,42 +7,42 @@ import Modal from '../../components/modal/modal.jsx';
 import { LoadingScreen, ErrorScreen } from '../../components/screens/';
 import styles from './profile.module.css';
 
-const links = [
-  {
-    to: '/profile',
-    text: 'Профиль',
-    description: 'В этом разделе вы можете изменить свои персональные данные',
-  },
-  {
-    to: '/profile/orders',
-    text: 'История заказов',
-    description: 'В этом разделе вы можете посмотреть свою историю заказов',
-  },
-];
-
 export default function ProfilePage() {
   const location = useLocation();
   const dispatch = useDispatch();
   const { request, error } = useSelector(state => state.auth);
-  const { description } = links.find(n => n.to === location.pathname) ?? {};
-  const onLogout = e => {
-    e.preventDefault();
-    dispatch(logout());
-  };
   const onCloseModal = () => dispatch(resetError());
+
+  const links = [
+    {
+      to: '/profile',
+      text: 'Профиль',
+      description: 'В этом разделе вы можете изменить свои персональные данные',
+    },
+    {
+      to: '/profile/orders',
+      text: 'История заказов',
+      description: 'В этом разделе вы можете посмотреть свою историю заказов',
+    },
+    {
+      to: '',
+      text: 'Выход',
+      onClick: e => {
+        e.preventDefault();
+        dispatch(logout());
+      },
+    },
+  ];
+
+  const { description } = links.find(n => n.to === location.pathname) ?? {};
 
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <nav className={styles.nav}>
-          {links.map(n => (
-            <div key={n.to} className={styles.link}>
-              <Link to={n.to}>{n.text}</Link>
-            </div>
+          {links.map(({ text, ...n }) => (
+            <Link key={n.to} className={styles.link} {...n}>{text}</Link>
           ))}
-          <div className={styles.link}>
-            <Link to="" onClick={onLogout}>Выход</Link>
-          </div>
         </nav>
         <div className={styles.description}>{description}</div>
       </div>
