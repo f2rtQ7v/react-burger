abe509602871163d734ac8f7754e4e2e30e62e12
Victor <xD34F@yandex.ru>
2025-02-10 22:01:03 +0300

    типизирован throttle

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,11 +1,14 @@
-export default function throttle(f, delay) {
+export default <T extends unknown[]>(
+  f: (...args: T) => void,
+  delay: number,
+) => {
   let lastCall = -Infinity;
 
-  return function() {
+  return (...args: T) => {
     const now = +new Date;
     if (now - lastCall > delay) {
       lastCall = now;
-      return f.apply(this, arguments);
+      f(...args);
     }
   };
 }
