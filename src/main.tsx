aed89bb86ecc9aaa221ffe79a2795eba51041a7a
Victor <xD34F@yandex.ru>
2025-02-11 02:00:39 +0300

    #root - точно есть

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import store from './services/store.ts';
 
 import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
