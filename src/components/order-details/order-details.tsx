ff1cb26d3ae84de6126442dd04a642612973abb0
Victor <xD34F@yandex.ru>
2025-02-11 06:02:00 +0300

    типизирован OrderDetails

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,21 +1,16 @@
-import PropTypes from 'prop-types';
 import styles from './order-details.module.css';
 import checkMarkImage from '../../images/check-mark.png';
 
-const OrderDetails = ({ orderId, ...props }) => (
-  <div className={styles.container} {...props}>
-    <div className="text text_type_digits-large">{orderId}</div>
-    <div className="m-5 text text_type_main-medium">идентификатор заказа</div>
-    <div className="m-10">
-      <img src={checkMarkImage} />
+export default function OrderDetails({ orderId }: { orderId: number }) {
+  return (
+    <div className={styles.container}>
+      <div className="text text_type_digits-large">{orderId}</div>
+      <div className="m-5 text text_type_main-medium">идентификатор заказа</div>
+      <div className="m-10">
+        <img src={checkMarkImage} />
+      </div>
+      <div className="m-2 text text_type_main-default">Заказ начали готовить</div>
+      <div className="m-2 text text_type_main-default text_color_inactive">Дождитесь готовности</div>
     </div>
-    <div className="m-2 text text_type_main-default">Заказ начали готовить</div>
-    <div className="m-2 text text_type_main-default text_color_inactive">Дождитесь готовности</div>
-  </div>
-);
-
-OrderDetails.propTypes = {
-  orderId: PropTypes.number.isRequired,
-};
-
-export default OrderDetails;
+  );
+}
