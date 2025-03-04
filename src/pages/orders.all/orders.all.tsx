62e81f77bb2aef5a5a83cf305bb061843435dfc9
Victor <xD34F@yandex.ru>
2025-03-04 11:18:33 +0300

    статистика заказов

--- src/pages/orders.all/orders.all.tsx
+++ src/pages/orders.all/orders.all.tsx
@@ -15,17 +15,54 @@ export default function OrdersPage() {
     return () => dispatch(actions.disconnect());
   }, [ dispatch ]);
 
-  const { orders } = useSelector(state => state.ordersAll);
+  const { orders, total, totalToday } = useSelector(state => state.ordersAll);
 
   if (!orders) {
     return <LoadingScreen />;
   }
+
+  const groupedOrders = orders.reduce((acc, n) => (
+    acc[+(n.status !== 'done')][0].push(n),
+    acc
+  ), [
+    [ [],   'Готовы:', styles.done ],
+    [ [], 'В работе:',          '' ],
+  ]);
+
+  const totals = [
+    [ 'Выполнено за всё время:', total ],
+    [ 'Выполнено за сегодня:', totalToday ],
+  ];
 
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>Лента заказов</h1>
       <div className={styles.content}>
         <Orders orders={orders} />
+        <div className={styles.statistics}>
+          <div className={styles.orderIds}>
+            {groupedOrders.map(([ ids, title, extraClass ]) => (
+              <div>
+                <div className={styles.title}>{title}</div>
+                <div className={styles.orderIdsListWrapper}>
+                  <ul
+                    className={styles.orderIdsList + ' ' + extraClass}
+                    style={{ columns: Math.ceil(ids.length / 5) }}
+                  >
+                    {ids.map(n => <li className={styles.orderIdsListItem}>{n.number}</li>)}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {totals.map(([ title, value ]) => (
+            <div className={styles.total}>
+              <div className={styles.title}>{title}</div>
+              <div className={styles.totalValue}>{value.toLocaleString()}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
